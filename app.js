42180c9ecab5d20757d1d950436383ec0eeea713
JulienRobberechts <robberechts.consulting@gmail.com>
2020-03-16 13:53:22 +0100

    :sparkles: handle CORS requests

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cors = require("cors");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var schedulesRouter = require('./routes/schedules');
+var schedulesRouter = require("./routes/schedules");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/schedules', schedulesRouter);
+app.use("/schedules", schedulesRouter);
 
 module.exports = app;
