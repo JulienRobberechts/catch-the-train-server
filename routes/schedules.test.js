281a357e8cb04a44f0c9f9b2656406b5979818cf
JulienRobberechts <robberechts.consulting@gmail.com>
2020-03-16 17:37:01 +0100

    :white_check_mark: replace snapshot tests

--- routes/schedules.test.js
+++ routes/schedules.test.js
@@ -11,17 +11,24 @@ describe("GET /schedules", () => {
   });
   it("should return the all schedules", async () => {
     const response = await api.get("/schedules").expect(200);
-    debug("/schedules body", response.body);
-    expect(response.body).toMatchSnapshot();
+    expect(response.body.routes.length).toBe(3);
   });
   it("should return the SGL schedules", async () => {
-    const response = await api.get("/schedules/SGL").expect(200);
-    debug("/schedules body", response.body);
-    expect(response.body).toMatchSnapshot();
+    const response = await api.get("/schedules/sgl").expect(200);
+    expect(response.body.routes.length).toBe(1);
   });
-  it("should return the SGL schedules", async () => {
-    const response = await api.get("/schedules/SGL/Paris").expect(200);
-    debug("/schedules body", response.body);
-    expect(response.body).toMatchSnapshot();
+  it("should return the SGL schedules with destination", async () => {
+    const response = await api
+      .get("/schedules/sgl/le-vesinet-centre")
+      .expect(200);
+    expect(response.body.routes.length).toBe(1);
+  });
+  it("should return all ML schedules", async () => {
+    const response = await api.get("/schedules/ml").expect(200);
+    expect(response.body.routes.length).toBe(2);
+  });
+  it("should return ML schedules for nation", async () => {
+    const response = await api.get("/schedules/ml/nation").expect(200);
+    expect(response.body.routes.length).toBe(1);
   });
 });
