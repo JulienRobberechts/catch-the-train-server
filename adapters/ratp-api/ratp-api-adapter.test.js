97b3ad2c84b1a17c15217b4a987d48deee400167
JulienRobberechts <robberechts.consulting@gmail.com>
2020-03-17 18:04:02 +0100

    :pencil: comments

--- adapters/ratp-api/ratp-api-adapter.test.js
+++ adapters/ratp-api/ratp-api-adapter.test.js
@@ -3,8 +3,10 @@ const apiAdapter = require("./ratp-api-adapter");
 describe("ratp-api adapter", () => {
   it("should return the all schedules", async () => {
     expect(1).toBe(1);
+    // TODO
   });
   it("should fail on connectivity error", async () => {
     expect(1).toBe(1);
+    // TODO
   });
 });
