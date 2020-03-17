d66eaa9e6ecad0f6b73dd73748076e4a809a728a
JulienRobberechts <robberechts.consulting@gmail.com>
2020-03-17 23:25:46 +0100

    :white_check_mark: test no connectivity

--- adapters/ratp-api/factory.js
+++ adapters/ratp-api/factory.js
@@ -1,6 +1,7 @@
-const RatpApiAdapter = process.env.SAMPLE_MODE
-  ? require("./ratp-api-adapter.mock")
-  : require("./ratp-api-adapter");
+const RatpApiAdapter =
+  process.env.SAMPLE_MODE === "true"
+    ? require("./ratp-api-adapter.mock")
+    : require("./ratp-api-adapter");
 const { ratpApiRootUrl } = require("../../config");
 
 const _apiAdapter = new RatpApiAdapter({ ratpApiRootUrl });
