861d9e4ce250a5f5917f071bcf506e805b8b6c04
JulienRobberechts <robberechts.consulting@gmail.com>
2020-04-09 14:34:20 +0200

    :truck: move all the src under a dir

--- src/routes/healthCheck.js
+++ src/routes/healthCheck.js
@@ -5,15 +5,15 @@ const debug = require("debug")("ctt:api:schedule");
 const { ValidationError } = require("../utils/errors");
 const { ConnectivityError } = require("../utils/errors");
 const getCurrentServerIp = require("../domains/config/getIp");
-const packageJson = require("../package.json");
+const packageJson = require("../../package.json");
 const config = require("../config");
 
-router.get("/", function(req, res, next) {
+router.get("/", function (req, res, next) {
   debug("ping check received");
   res.send({ status: "ok" });
 });
 
-router.get("/parameters", async function(req, res, next) {
+router.get("/parameters", async function (req, res, next) {
   const serverPublicIp = await getCurrentServerIp();
   const { version } = packageJson;
   const ratpApiRootUrl = config.RATP_API_ROOT_URL;
@@ -23,26 +23,26 @@ router.get("/parameters", async function(req, res, next) {
     serverPublicIp,
     version,
     ratpApiRootUrl,
-    ratpApiMockData
+    ratpApiMockData,
   });
 });
 
 router.get(
   "/error-async",
-  wrapAsync(async function(req, res, next) {
+  wrapAsync(async function (req, res, next) {
     debug("error-async check received");
     throw Error("sample asynchronous error message");
   })
 );
 
-router.get("/error-sync", function(req, res, next) {
+router.get("/error-sync", function (req, res, next) {
   debug("error-sync check received");
   throw Error("sample synchronous error message");
 });
 
 router.get(
   "/error-validation",
-  wrapAsync(async function(req, res, next) {
+  wrapAsync(async function (req, res, next) {
     debug("error-validation check received");
     throw new ValidationError("sample validation error message");
   })
@@ -50,7 +50,7 @@ router.get(
 
 router.get(
   "/error-connectivity",
-  wrapAsync(async function(req, res, next) {
+  wrapAsync(async function (req, res, next) {
     debug("error-connectivity check received");
     throw new ConnectivityError("sample connectivity error message");
   })
