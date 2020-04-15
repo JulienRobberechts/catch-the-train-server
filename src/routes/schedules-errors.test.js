c04f0680f9ac30f956f34e5334f7832be39dc3d4
JulienRobberechts <robberechts.consulting@gmail.com>
2020-04-15 08:42:36 +0200

    :white_check_mark: split test files

--- src/routes/schedules-errors.test.js
+++ src/routes/schedules-errors.test.js
@@ -4,38 +4,11 @@ const debug = require("debug")("ctt:test");
 const app = require("../app");
 const api = request(app);
 const {
-  mockApiCalls,
   mockApiCallsWithNoConnectivity,
   mockApiCallsWithScheduleUnavailable,
 } = require("../tests/mock/ratp-api/mockApiCalls");
 
 const config = require("../config");
-
-describe("GET /next-train/{network}/{line}/{station}?missions=...", () => {
-  beforeEach(() => {
-    if (config.RATP_API_MOCK_DATA)
-      throw Error("RATP_API_MOCK_DATA should be set to 'false' for this test");
-    mockApiCalls();
-  });
-  it("should return all missions for chatelet", async () => {
-    const response = await api
-      .get("/next-trains/rers/A/chatelet+les+halles")
-      .expect(200);
-    expect(response.body.departures.length).toBe(8);
-  });
-  it("should return 'UPAC' missions for chatelet", async () => {
-    const response = await api
-      .get("/next-trains/rers/A/chatelet+les+halles?missions=UPAC")
-      .expect(200);
-    expect(response.body.departures.length).toBe(2);
-  });
-  it("should return 'UPAC' and 'ZEBU' missions for chatelet", async () => {
-    const response = await api
-      .get("/next-trains/rers/A/chatelet+les+halles?missions=UPAC,ZEBU")
-      .expect(200);
-    expect(response.body.departures.length).toBe(5);
-  });
-});
 
 describe("No connectivity", () => {
   beforeEach(() => {
---
+++ src/routes/schedules-happyPath.test.js
@@ -0,0 +1,34 @@
+const request = require("supertest");
+const debug = require("debug")("ctt:test");
+
+const app = require("../app");
+const api = request(app);
+const { mockApiCalls } = require("../tests/mock/ratp-api/mockApiCalls");
+
+const config = require("../config");
+
+describe("GET /next-train/{network}/{line}/{station}?missions=...", () => {
+  beforeEach(() => {
+    if (config.RATP_API_MOCK_DATA)
+      throw Error("RATP_API_MOCK_DATA should be set to 'false' for this test");
+    mockApiCalls();
+  });
+  it("should return all missions for chatelet", async () => {
+    const response = await api
+      .get("/next-trains/rers/A/chatelet+les+halles")
+      .expect(200);
+    expect(response.body.departures.length).toBe(8);
+  });
+  it("should return 'UPAC' missions for chatelet", async () => {
+    const response = await api
+      .get("/next-trains/rers/A/chatelet+les+halles?missions=UPAC")
+      .expect(200);
+    expect(response.body.departures.length).toBe(2);
+  });
+  it("should return 'UPAC' and 'ZEBU' missions for chatelet", async () => {
+    const response = await api
+      .get("/next-trains/rers/A/chatelet+les+halles?missions=UPAC,ZEBU")
+      .expect(200);
+    expect(response.body.departures.length).toBe(5);
+  });
+});
