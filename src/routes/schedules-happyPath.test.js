36bbd688dfb24cbcd0b0dcd0ef5c23bcac8e4bae
JulienRobberechts <robberechts.consulting@gmail.com>
2020-06-07 08:54:33 +0200

    :sparkles: missions calculation on the server v1

--- src/routes/schedules-happyPath.test.js
+++ src/routes/schedules-happyPath.test.js
@@ -17,29 +17,40 @@ describe("GET /next-train/{network}/{line}/{station}?missions=...", () => {
   });
   it("should return all missions for chatelet", async () => {
     const response = await api.get(url).expect(200);
+    expect(response.body.departures).toMatchSnapshot();
     expect(response.body.departures.length).toBe(8);
   });
   it("should return 'UPAC' missions for chatelet", async () => {
     const response = await api.get(url + "?missions=UPAC").expect(200);
+    expect(response.body.departures).toMatchSnapshot();
     expect(response.body.departures.length).toBe(2);
   });
   it("should return 'UPAC' and 'ZEBU' missions for chatelet", async () => {
     const response = await api.get(url + "?missions=UPAC,ZEBU").expect(200);
+    expect(response.body.departures).toMatchSnapshot();
     expect(response.body.departures.length).toBe(5);
   });
 });
 
 describe("GET schedule by destination", () => {
   const fromStation = "chatelet+les+halles";
-  const toStation = "le+vesinet+le+pecq";
-  const url = `/next-trains/rers/A/${fromStation}/${toStation}`;
   beforeEach(() => {
     if (config.RATP_API_MOCK_DATA)
       throw Error("RATP_API_MOCK_DATA should be set to 'false' for this test");
     mockApiCalls(fromStation);
   });
-  it("should return departure towards auber for chatelet", async () => {
+  it("should return 5 departures from chatelet+les+halles to maisons+laffitte", async () => {
+    const toStation = "maisons+laffitte";
+    const url = `/next-trains/rers/A/${fromStation}/${toStation}`;
     const response = await api.get(url).expect(200);
+    expect(response.body.departures).toMatchSnapshot();
     expect(response.body.departures.length).toBe(5);
+  });
+  it("should return 2 departures from chatelet+les+halles to cergy+le+haut", async () => {
+    const toStation = "cergy+le+haut";
+    const url = `/next-trains/rers/A/${fromStation}/${toStation}`;
+    const response = await api.get(url).expect(200);
+    expect(response.body.departures).toMatchSnapshot();
+    expect(response.body.departures.length).toBe(2);
   });
 });
