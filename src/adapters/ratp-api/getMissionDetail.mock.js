713c51b676576d59992374bd2dddda986fd893ed
JulienRobberechts <robberechts.consulting@gmail.com>
2020-06-09 22:11:44 +0200

    :art: getMissionDetail mock

--- src/adapters/ratp-api/getMissionDetail.mock.js
+++ src/adapters/ratp-api/getMissionDetail.mock.js
@@ -1,10 +1,9 @@
+const {
+  getMissionDetailsMock,
+} = require("../../tests/mock/ratp-api/mockMissionsCalls");
+
 async function getMissionDetail({ missionCode }) {
-  try {
-    const missionDetail = require(`../../data/ratp/rers/A/by-mission/stations-${missionCode}.json`);
-    return missionDetail.result;
-  } catch (err) {
-    console.log(`Mission '${missionCode}' can not be mocked`);
-  }
+  return getMissionDetailsMock(missionCode);
 }
 
 module.exports = getMissionDetail;
--- src/tests/mock/ratp-api/mockMissionsCalls.js
+++ src/tests/mock/ratp-api/mockMissionsCalls.js
@@ -6,28 +6,50 @@ const fs = require("fs");
 const readdir = util.promisify(fs.readdir);
 const missionsDirPath = path.join(__dirname, "data\\missions");
 
+const getMissionDetailsMock = (missionCode) => {
+  try {
+    const missionDetailMock = require(`./data/missions/stations-${missionCode}.json`);
+    return missionDetailMock;
+  } catch (err) {
+    console.log(`Mission '${missionCode}' will be mocked with empty schedule`);
+    return {
+      result: {
+        stations: [],
+      },
+      _metadata: {
+        fake: true,
+        call: `GET /missions/rers/A/${missionCode}`,
+        date: "2020-01-01T00:00:00+00:00",
+        version: 4,
+      },
+    };
+  }
+};
+
+const getMissionFromFileName = (filename) =>
+  filename.replace("stations-", "").replace(".json", "");
+
 const nockRatpMissionsApiCalls = async () => {
   try {
     const files = await readdir(missionsDirPath);
     files.forEach(function (filename) {
-      const missionCode = filename
-        .replace("stations-", "")
-        .replace(".json", "");
-      const missionDetail = require(`./data/missions/${filename}`);
-      nockMission(missionCode, missionDetail);
+      const missionCode = getMissionFromFileName(filename);
+      const missionSchedule = getMissionDetailsMock(missionCode);
+      nockMission(missionCode, missionSchedule);
     });
   } catch (err) {
     return console.log(`Unable to scan directory: ${missionsDirPath}` + err);
   }
 };
 
-const nockMission = (missionCode, missionDetail) => {
+const nockMission = (missionCode, missionSchedule) => {
   nock(config.RATP_API_ROOT_URL)
     .defaultReplyHeaders({ "access-control-allow-origin": "*" })
     .get(`/missions/rers/A/${missionCode}`)
-    .reply(200, missionDetail);
+    .reply(200, missionSchedule);
 };
 
 module.exports = {
   nockRatpMissionsApiCalls,
+  getMissionDetailsMock,
 };
