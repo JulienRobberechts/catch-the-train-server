6139e8376c8a0b23d945425efee1b6094d4d77ba
JulienRobberechts <robberechts.consulting@gmail.com>
2020-06-17 09:41:30 +0200

    :white-check-mark: test for getSchedulesForASpecialMission - prepare data

--- src/data/ratp/rers/A/script.js
+++ src/data/ratp/rers/A/script.js
@@ -2,20 +2,16 @@ const stationDraft = require("./stations.json");
 const fs = require("fs");
 
 function onlyUnique(item, index, sourceArray) {
-  return sourceArray.findIndex(i => i.slug === item.slug) === index;
+  return sourceArray.findIndex((i) => i.slug === item.slug) === index;
 }
 
-var stationUnique = stationDraft.filter(onlyUnique);
+var stationUnique = stationDraft.filter(onlyUnique).map((s) => s.slug);
 
-fs.writeFile(
-  "./data/ratp/rers/A/stationUnique.json",
-  JSON.stringify(stationUnique),
-  function(err) {
-    if (err) {
-      return console.log(err);
-    }
-    console.log("stationUnique file saved");
+fs.writeFile("./station-modif.json", JSON.stringify(stationUnique), function (
+  err
+) {
+  if (err) {
+    return console.log(err);
   }
-);
-
-// console.log(stationUnique);
+  console.log("station-modif file saved");
+});
--- src/domains/ratp/missions/missionScheduleSpecial.test.js
+++ src/domains/ratp/missions/missionScheduleSpecial.test.js
@@ -1,6 +1,66 @@
 const { getSchedulesForASpecialMission } = require("./missionScheduleSpecial");
 const each = require("jest-each").default;
 
+const sectionA3 = [
+  "cergy+le+haut",
+  "cergy+st+christophe",
+  "cergy+prefecture",
+  "neuville+universite",
+  "conflans+fin+d'oise",
+  "acheres+ville",
+];
+
+const sectionA35 = ["maisons+laffitte", "sartrouville", "houilles"];
+
+const sectionA0 = [
+  "nanterre+prefecture",
+  "grande+arche+la+defense",
+  "charles+de+gaulle+etoile",
+  "auber",
+  "chatelet+les+halles",
+  "gare+de+lyon",
+  "nation",
+  "vincennes",
+];
+
+const sectionA2 = [
+  "fontenay+sous+bois",
+  "nogent+sur+marne",
+  "joinville+le+pont",
+  "saint+maur+creteil",
+  "le+parc+de+saint+maur",
+  "champigny",
+  "la+varenne+chennevieres",
+  "sucy+bonneuil",
+  "boissy+saint+leger",
+];
+
+const sectionA5 = ["poissy", "acheres+grand+cormier"];
+
+const sectionA1 = [
+  "st+germain+en+laye",
+  "le+vesinet+le+pecq",
+  "le+vesinet+centre",
+  "chatou+croissy",
+  "rueil+malmaison",
+  "nanterre+ville",
+  "nanterre+universite",
+];
+
+const sectionA4 = [
+  "val+de+fontenay",
+  "neuilly+plaisance",
+  "bry+sur+marne",
+  "noisy+le+grand+mont+d'est",
+  "noisy+champs",
+  "noisiel",
+  "lognes",
+  "torcy",
+  "bussy+st+georges",
+  "val+d'europe+serris",
+  "marne+la+vallee+chessy",
+];
+
 const stationsYOZZ = ["a", "b", "c"];
 const stationsTOZZ = ["c", "b", "a"];
 const stationsBNZZ = ["c", "b", "a"];
@@ -8,6 +68,7 @@ const stationsBNZZ = ["c", "b", "a"];
 const missions = [
   ["YOZZ", stationsYOZZ],
   ["TOZZ", stationsTOZZ],
+  ["BNZZ", stationsBNZZ],
 ];
 
 describe("Missions schedule Special", () => {
