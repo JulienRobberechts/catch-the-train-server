a2cecc420e2c511ff02aef2d82feeb0f1a0c4847
JulienRobberechts <robberechts.consulting@gmail.com>
2020-07-08 21:48:44 +0200

    :white_check_mark: test all stations

--- src/tests/live/live-schedule.test.js
+++ src/tests/live/live-schedule.test.js
@@ -14,36 +14,34 @@ const stationNogent = "nogent+sur+marne";
 const stationStMaur = "le+parc+de+saint+maur";
 const stationAuber = "auber";
 
+const stations = require("../../data/ratp/rers/A/stations.json");
+
 const { handleError } = require("../../utils/errors/errorManagement");
 
 describe("live schedule", () => {
   it("should show the time", async () => {
     console.log("live schedule :>> ", "live schedule");
   });
-  each([
-    [stationChatelet, stationAuber],
-    [stationNanterrePrefecture, stationAuber],
-    [stationNanterreVille, stationAuber],
-    [stationChatou, stationAuber],
-    [stationStGermain, stationAuber],
-    [stationNogent, stationAuber],
-    [stationStMaur, stationAuber],
-  ]).it(
+  const stationsTestCases = stations
+    .filter((s) => s.owner === "ratp")
+    .map((s) => [s.name, s]);
+  each([...stationsTestCases]).it(
     "should request live schedule for %s without errors",
-    async (src, dest) => {
+    async (name, station) => {
       const schedulesController = getSchedulesController();
-
+      const dest =
+        stationAuber === station.slug ? stationChatelet : stationAuber;
       try {
         const result = await schedulesController.getSchedulesForJourneyByDestination(
           network,
           line,
-          src,
+          station.slug,
           dest
         );
       } catch (err) {
         const formattedError = handleError(err);
         console.log(
-          `live schedule Error :>> ${src}`,
+          `live schedule Error :>> ${station.slug}`,
           JSON.stringify(formattedError, null, 2)
         );
         fail("live schedule Error");
