95a4424a6c12e18e3109454173ccb8d5a4808eb4
JulienRobberechts <robberechts.consulting@gmail.com>
2020-07-08 08:49:56 +0200

    :white_check_mark: increase scope of live check

--- src/tests/live/live-schedule.test.js
+++ src/tests/live/live-schedule.test.js
@@ -1,33 +1,56 @@
 require("custom-env").env("livetest");
 const { getSchedulesController } = require("../../controllers/factory");
+const each = require("jest-each").default;
 
 const network = "rers";
 const line = "A";
+
 const stationChatelet = "chatelet+les+halles";
+const stationNanterrePrefecture = "nanterre+prefecture";
+const stationNanterreVille = "nanterre+ville";
+const stationChatou = "chatou+croissy";
+const stationStGermain = "st+germain+en+laye";
+const stationNogent = "nogent+sur+marne";
+const stationStMaur = "le+parc+de+saint+maur";
 const stationAuber = "auber";
+
 const { handleError } = require("../../utils/errors/errorManagement");
 
 describe("live schedule", () => {
   it("should show the time", async () => {
     console.log("live schedule :>> ", "live schedule");
   });
-  it("should request live schedule for chatelet without errors", async () => {
-    const schedulesController = getSchedulesController();
+  each([
+    [stationChatelet, stationAuber],
+    [stationNanterrePrefecture, stationAuber],
+    [stationNanterreVille, stationAuber],
+    [stationChatou, stationAuber],
+    [stationStGermain, stationAuber],
+    [stationNogent, stationAuber],
+    [stationStMaur, stationAuber],
+  ]).it(
+    "should request live schedule for %s without errors",
+    async (src, dest) => {
+      const schedulesController = getSchedulesController();
 
-    try {
-      const result = await schedulesController.getSchedulesForJourneyByDestination(
-        network,
-        line,
-        stationChatelet,
-        stationAuber
-      );
-    } catch (err) {
-      const publicError = handleError(err);
-      console.log(
-        "live schedule Error :>> ",
-        JSON.stringify(publicError, null, 2)
-      );
-      fail(JSON.stringify(publicError, null, 2));
+      try {
+        const result = await schedulesController.getSchedulesForJourneyByDestination(
+          network,
+          line,
+          src,
+          dest
+        );
+      } catch (err) {
+        const publicError = handleError(err);
+        console.log(
+          `live schedule Error :>> ${src}-${dest}`,
+          JSON.stringify(publicError, null, 2)
+        );
+        fail(
+          `live schedule Error :>> ${src}-${dest}` +
+            JSON.stringify(publicError, null, 2)
+        );
+      }
     }
-  });
+  );
 });
